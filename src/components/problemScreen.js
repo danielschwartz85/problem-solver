8ce74b8f30f0997fe41ac961766ee6aedfc2d88d
daniel <danielsch24@gmail.com>
2018-03-07 17:14:21 +0200

    added send problem option

--- src/components/problemScreen.js
+++ src/components/problemScreen.js
@@ -12,6 +12,7 @@ import MoreVertIcon from 'material-ui-icons/MoreVert';
 import { ListItemIcon, ListItemText } from 'material-ui/List';
 import DeleteForever from 'material-ui-icons/DeleteForever';
 import Edit from 'material-ui-icons/Edit';
+import Send from 'material-ui-icons/Email';
 import { PAGES } from '../actions/routingActions';
 
 const styles = theme => ({
@@ -60,6 +61,29 @@ class ProblemScreen extends React.Component {
     this.props.deleteAndShowWelcome(this.props.selectedProblemId);
   }
 
+  // NOTE: Temp feature
+  handleSend = () => {
+    const { problem } = this.props;
+    Utils.transformationSentence(problem);
+    const past = problem.pastVerbs.reduce((acc,i) => (
+      acc += `, ${problem.verbs[i]}`
+    ), "");
+    const negative = problem.negativeVerbs.reduce((acc,i) => (
+      acc += `, ${problem.verbs[i]}`
+    ), "");
+    let mailBody = `בעיה: ${problem.description}\n`;
+    mailBody += `פעלים: ${problem.verbs.join(',')}\n`;
+    mailBody += `פעלי עבר: ${past}\nפעלים עם קונוטצייה שלילית: ${negative}\n`;
+    mailBody += `פעלי אתחול: ${Utils.transformationSentence(problem)}\n`;
+    mailBody += `שם: ${problem.name}\nשם חדש: ${problem.newName}\n`;
+    mailBody += `עבר דומינו: ${problem.pastDomino}\n`;
+    mailBody += `עתיד דומינו: ${problem.futureDomino}\n`;
+    mailBody += `פתרון בעיה: ${problem.problemPlanted}`;
+    const subject = `הבעיה של ${problem.name}`;
+    const lString = `mailto:berya3@gmail.com?subject=${subject}&body=${mailBody}`;
+    window.location = lString.replace(/\n/g, escape('\r\n')+escape('\r\n'));
+  }
+
   render() {
     const { classes, problem } = this.props;
     const { anchorEl } = this.state;
@@ -92,6 +116,12 @@ class ProblemScreen extends React.Component {
               <DeleteForever />
             </ListItemIcon>
             <ListItemText inset primary={Config.problemScreen.deleteText} />
+          </MenuItem>
+          <MenuItem onClick={this.handleSend}>
+            <ListItemIcon>
+              <Send/>
+            </ListItemIcon>
+            <ListItemText inset primary={'שלח'} />
           </MenuItem>
         </Menu>
       </div>
