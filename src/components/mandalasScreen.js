bf54410794c35d95a3a6999bca1858c4c37b3665
Daniel Schwartz <danielsch24@gmail.com>
2020-06-06 20:46:43 +0300

    faster shuffel speed

--- src/components/mandalasScreen.js
+++ src/components/mandalasScreen.js
@@ -8,7 +8,7 @@ class MandalasScreen extends React.Component {
     return (
       <White>
         <Grid container spacing={1}>
-          <MandalaCard shuffelSpeed={300} />
+          <MandalaCard shuffelSpeed={150} />
           <Grid item xs={12} sm={12}></Grid>
         </Grid>
       </White>
